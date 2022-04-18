9d7f3785908b5b6a3bc29acce0e6c5992f9158b4
JongwonHyun <190otomebass.gen@gmail.com>
2022-04-18 11:56:47 +0900

    strategy pattern example

---
+++ src/test/java/StrategyTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+import strategy.AbcBankRemit;
+import strategy.DefBankRemit;
+import strategy.Remit;
+import strategy.fee.DefaultFee;
+import strategy.fee.Fee;
+import strategy.fee.PartnershipFee;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StrategyTest {
+
+    @Test
+    void StrategyRun() {
+        final var amount = 1000;
+        final var defaultFee =  amount * Fee.DEFAULT_FEE;
+        final var partnershipFee = amount * Fee.PARTNERSHIP_FEE;
+
+        Remit abcBankRemit = new AbcBankRemit(new DefaultFee());
+        Remit defBankRemit = new DefBankRemit(new PartnershipFee());
+
+        final var amountAbc = abcBankRemit.calculateAmount(amount);
+        final var amountDef = defBankRemit.calculateAmount(amount);
+
+        assertEquals(amount + defaultFee, amountAbc);
+        assertEquals(amount + partnershipFee, amountDef);
+    }
+}
