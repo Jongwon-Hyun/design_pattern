c188b06c532d82e2a85c69331a1abb1d7a6145e2
JongwonHyun <190otomebass.gen@gmail.com>
2022-05-04 22:00:16 +0900

    command pattern

---
+++ command/transfer.go
@@ -0,0 +1,22 @@
+package command
+
+type transfer struct {
+	command func(transferParam TransferParam)
+}
+
+func NewTransfer(command func(transferParam TransferParam)) *transfer {
+	return &transfer{command: command}
+}
+
+func (t *transfer) Start(transferParam TransferParam) {
+	t.command(transferParam)
+}
+
+type TransferParam struct {
+	amount  float64
+	account string
+}
+
+func NewTransferParam(amount float64, account string) TransferParam {
+	return TransferParam{amount: amount, account: account}
+}
